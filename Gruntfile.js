a6a3dd0cef13b3c1731c62f9a95c1b4216345cdf
Michael Lajlev <lajlev@gmail.com>
2014-03-06 15:39:25 +0100

    readme updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,11 +6,11 @@ module.exports = function(grunt) {
       basic_and_extras: {
         files: {
           'source/js/foundation-bower.js': [
-            '../bower_components/jquery/jquery.js',
-            '../bower_components/foundation/js/foundation.min.js'
+            'bower_components/jquery/jquery.js',
+            'bower_components/foundation/js/foundation.min.js'
           ],
           'source/js/modernizr-bower.js': [
-            '../bower_components/modernizr/modernizr.js'
+            'bower_components/modernizr/modernizr.js'
           ]
         }
       }
