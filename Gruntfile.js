84acfad493aacc22f62d6f9125589372549f2573
Emil Ankersen <emilankersen88@gmail.com>
2014-03-13 12:56:02 +0100

    update

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    concat: {
+      basic_and_extras: {
+        files: {
+          'source/js/foundation-bower.js': [
+            'bower_components/jquery/jquery.js',
+            'bower_components/foundation/js/foundation.min.js'
+          ],
+          'source/js/modernizr-bower.js': [
+            'bower_components/modernizr/modernizr.js'
+          ]
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-concat');
+
+  grunt.registerTask('default', ['concat']);
+}
