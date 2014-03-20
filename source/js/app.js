c1e782fd07e26b636d025cc9a026ab45097cbcb8
Emil Ankersen <emilankersen88@gmail.com>
2014-03-20 11:17:26 +0100

    ændret statements til produkt

--- source/js/app.js
+++ source/js/app.js
@@ -15,13 +15,63 @@ $(document).foundation({
 });
 
 $(document).ready(function(){
+  
+// TOGGLE MENU
   $(".toggle_menu a").click(function(){
     $(".drop_down").slideToggle("slow");
-  });
+});
+
+
+// READ MORE TOGGLE
+$(".read_more").click(function(){
+    $(".read_more_toggle").slideToggle("slow");  
+});
+
+// SLIDE TO DIV
 
   $("[id*='Btn']").stop(true).on('click', function (e) {e.preventDefault();$(this).scrolld();})
 });
 
+// jQuery(document).ready(function() {
+//     var offset = 220;
+//     var duration = 800;
+//     jQuery(window).scroll(function() {
+//         if (jQuery(this).scrollTop() > offset) {
+//             jQuery('.back-to-top').fadeIn(duration);
+//         } else {
+//             jQuery('.back-to-top').fadeOut(duration);
+//         }
+//     });
+    
+//     jQuery('.back-to-top').click(function(event) {
+//         event.preventDefault();
+//         jQuery('html, body').animate({scrollTop: 0}, duration);
+//         return false;
+//     })
+// });
 
+// jQuery(document).ready(function() {
+//     var offset = 100;
+//     var duration = 1000;
+//     jQuery(window).scroll(function() {
+//         if (jQuery(this).scrollTop() > offset) {
+//             jQuery('.fade_in_h1').fadeOut(duration);
+//         } else {
+//             jQuery('.fade_in_h1').fadeIn(duration);
+//         }
+//     });
+// });
+
+// jQuery(document).ready(function() {
+//     var offset = 80;
+//     var duration = 1000;
+//     jQuery(window).scroll(function() {
+//         if (jQuery(this).scrollTop() > offset) {
+//             jQuery('.fade_in_img').fadeOut(duration);
+//         } else {
+//             jQuery('.fade_in_img').fadeIn(duration);
+//         }
+//     });
+// });
 
 
--- source/text/_produkt.md
+++ source/text/_produkt.md
@@ -4,4 +4,7 @@ Siden 2004 har vi arbejdet for at gøre det lettere for udsatte børn og unge at
 I årenes løb har vi hjulpet mange andre i gang med digital rådgivning, f.eks. TUBA og Bakkevej.
 Der er mange faldgruber og udfordringer når virksomheder og organisationer bevæger sig ud på nettet og møder deres brugere @todo skriv mere.
 
+<div class="read_more_toggle"><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Suscipit, ab porro sunt dignissimos natus consequuntur debitis perferendis earum quas quae quis sint culpa nulla modi provident doloremque esse harum ipsam sed tempore nostrum ad ut officiis. Enim, explicabo porro inventore esse aliquid consequuntur qui aliquam beatae architecto vitae pariatur tempore reprehenderit atque dolores rem. Error, voluptates, tenetur, reprehenderit sapiente assumenda aspernatur ducimus facilis adipisci explicabo debitis amet placeat autem accusantium perferendis veritatis velit maiores minus magnam itaque neque eos enim harum expedita rem tempora odio similique. Numquam, nemo, ipsam in perferendis repudiandae quam corrupti consectetur similique officiis pariatur est placeat!</p></div>
+
+
 <div class="button read_more">Læs mere</div>
